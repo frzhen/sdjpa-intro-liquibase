ecce409da77fa6fd783c901e82ea29747045347a
Fred Zhen <13911239075@163.com>
2021-11-20 21:47:06 +0800

    Added test sequence with @TestMethodOrder and pass the test data between tests using @Commit to stop rollback.

--- src/test/java/guru/ysy/sdjpaintro/SpringBootJpaTestSlice.java
+++ src/test/java/guru/ysy/sdjpaintro/SpringBootJpaTestSlice.java
@@ -2,29 +2,46 @@ package guru.ysy.sdjpaintro;
 
 import guru.ysy.sdjpaintro.domain.Book;
 import guru.ysy.sdjpaintro.repositories.BookRepository;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.test.annotation.Rollback;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 /**
  * Created by zhenrui on 2021/11/20 18:41
  */
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @DataJpaTest
 public class SpringBootJpaTestSlice {
 
     @Autowired
     BookRepository bookRepository;
 
+//    @Rollback(value = false)//stop rollback so that the state can be passed on to the next test
+    @Commit//This is cleaner than @Rollback
+    @Order(1)
     @Test
     void testJpaTestSplice() {
         long countBefore = bookRepository.count();
+        assertThat(countBefore).isEqualTo(0);
 
         bookRepository.save(new Book("My Book", "1234567", "Self"));
 
         long countAfter = bookRepository.count();
 
         assertThat(countBefore).isLessThan(countAfter);
+    }
+
+    @Order(2)
+    @Test
+    void testJpaTestSpliceTransaction() {
+        long countBefore = bookRepository.count();
+        assertThat(countBefore).isEqualTo(1);
     }
 }
