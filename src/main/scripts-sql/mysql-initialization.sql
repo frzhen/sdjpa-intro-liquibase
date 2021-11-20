057c6102e7481e2dbfcb67fa2842d849d73b4c2f
Fred Zhen <13911239075@163.com>
2021-11-20 15:15:34 +0800

    updated initial MySQL setup script.

--- src/main/scripts-sql/mysql-initialization.sql
+++ src/main/scripts-sql/mysql-initialization.sql
@@ -1,9 +1,9 @@
 DROP DATABASE IF EXISTS bookdb;
-DROP USER IF EXISTS `bookadmin`@`localhost`;
-DROP USER IF EXISTS `bookuser`@`localhost`;
+DROP USER IF EXISTS `bookadmin`@`%`;
+DROP USER IF EXISTS `bookuser`@`%`;
 CREATE DATABASE IF NOT EXISTS bookdb CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-CREATE USER IF NOT EXISTS `bookadmin`@`localhost` IDENTIFIED WITH mysql_native_password BY 'password';
+CREATE USER `bookadmin`@`localhost` IDENTIFIED BY 'password';
 GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, REFERENCES, INDEX, ALTER, EXECUTE, CREATE VIEW, SHOW VIEW, CREATE ROUTINE, ALTER ROUTINE, EVENT, TRIGGER ON `bookdb`.* to `bookadmin`@`localhost`;
-CREATE USER IF NOT EXISTS `bookuser`@`localhost` IDENTIFIED WITH mysql_native_password BY 'password';
-GRANT SELECT, INSERT, UPDATE, DELETE, SHOW VIEW ON `bookdb`.* TO `bookuser`@`localuser`;
+CREATE USER `bookuser`@`localhost` IDENTIFIED BY 'password';
+GRANT SELECT, INSERT, UPDATE, DELETE, SHOW VIEW ON `bookdb`.* TO `bookuser`@`localhost`;
 FLUSH PRIVILEGES;
